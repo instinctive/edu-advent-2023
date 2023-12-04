89326c760607791dff13e999abf7692b0951affd
instinctive <177694+instinctive@users.noreply.github.com>
2023-12-03 19:42:12 -0800

    Day 3 complete solution

--- day03.hs
+++ day03.hs
@@ -2,34 +2,53 @@
 
 module Main where
 
+import Control.Lens
 import Data.Array.IArray ((!))
 import Data.Array.ST
+import Data.Containers.ListUtils ( nubOrd )
 import Data.List.Split ( wordsBy )
-import Data.Map.Strict ( Map )
-import Data.Text       ( Text )
-import qualified Data.Text    as T
-import qualified Data.Text.IO as T
+import qualified Data.Map.Strict as M
+import qualified Data.Set        as S
+import qualified Data.Text       as T
+import qualified Data.Text.IO    as T
 
 main = T.getContents <&> T.lines >>= \tt -> do
-    print $ part1 tt
+    each print $ solve tt
 
-part1 tt = sum
-    [ read @Int w
-    | (row,t) <- zip [0..] tt
-    , let xx = zip (T.unpack t) (partRow row)
-    , (w,bb) <- unzip <$> wordsBy (not.isDigit.fst) xx
-    , or bb
-    ]
+modifyArray ary k f = readArray ary k >>= writeArray ary k . f
+
+solve tt =
+    (part1,part2)
   where
     nrows = length tt
     ncols = T.length (head tt)
-    parts = runSTUArray do
-        ary <- newArray ((-1,-1),(nrows,ncols)) False
-        let setbit r c dr dc = writeArray ary (r+dr,c+dc) True
+
+    parts = runSTArray do
+        ary <- newArray ((-1,-1),(nrows,ncols)) []
+        let setbit r c dr dc = modifyArray ary (r+dr,c+dc) ((r,c):)
         for_ (zip [0..] tt) \(row,t) ->
             for_ [0..ncols-1] \col -> do
                 let c = T.index t col
                 when (c /= '.' && not (isDigit c)) do
                     sequence_ $ setbit row col <$> [-1..1] <*> [-1..1]
         pure ary
-    partRow r = (parts!).(r,) <$> [0..ncols-1]
+
+    partsAndGears =
+        [ (read @Int w, nubOrd $ concat bb)
+        | (row,t) <- zip [0..] tt
+        , let xx = zip (T.unpack t) $ (parts!).(row,) <$> [0..ncols-1]
+        , (w,bb) <- unzip <$> wordsBy (not.isDigit.fst) xx
+        , any (not.null) bb
+        ]
+
+    part1 = sum $ fst <$> partsAndGears
+
+    part2
+        = concatMap invert partsAndGears
+        & M.fromListWith (<>)
+        & M.filter twoItems
+        & sum . fmap product
+      where
+        invert (part,gears) = (,[part]) <$> gears
+        twoItems [_,_] = True
+        twoItems _     = False
