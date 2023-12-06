b6e21ef6d4b10cbd45025fbc08674ba880526f69
instinctive <177694+instinctive@users.noreply.github.com>
2023-12-06 10:27:59 -0800

    Day 6 complete - linear

---
+++ day06.hs
@@ -0,0 +1,16 @@
+-- https://adventofcode.com/2023/day/6
+
+module Main where
+
+main = tgetContents <&> map (tail.twords) . tlines >>= \pp -> do
+    print $ part1 pp
+    print $ part2 pp
+
+part1 [tt,dd] = fmap product . sequence $
+    zipWith solve (tdecimal <$> tt) (tdecimal <$> dd)
+
+part2 [tt,dd] = solve (tdecimal $ mconcat tt) (tdecimal $ mconcat dd)
+
+solve t d = find pred [1..] <&> answer where
+    pred i = i * (t-i) > d
+    answer i = t - 2*i + 1
