f03f7d438456f5df9f786178a3c65617be59217f
instinctive <177694+instinctive@users.noreply.github.com>
2023-12-08 21:29:38 -0800

    Day 9 solution

---
+++ day09.hs
@@ -0,0 +1,16 @@
+-- https://adventofcode.com/2023/day/9
+
+module Main where
+
+main = tgetContents <&> parse >>= \xxx -> do
+    print $ solve last (+) xxx
+    print $ solve head (-) xxx
+
+parse = map (map tsigned . twords) . tlines
+
+solve acc op = sum . map go where
+    go xx | all (==0) xx = 0
+    go xx =
+        op (acc xx) (go diff)
+      where
+        diff = zipWith (-) (tail xx) xx
