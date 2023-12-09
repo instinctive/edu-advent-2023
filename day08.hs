f2be2fa40aaa3fe4064f9cc26095160079872808
instinctive <177694+instinctive@users.noreply.github.com>
2023-12-09 06:44:07 -0800

    Fast code slow algorithm

--- day08.hs
+++ day08.hs
@@ -6,27 +6,60 @@ module Main where
 
 import qualified Data.Map.Strict as M
 
-main = tgetContents <&> parse . tlines >>= \(inst,nodes) -> do
-    let next i x = fromJust (M.lookup x nodes) & \(l,r) ->
-            case tindex inst (i `mod` tlength inst) of
-                'L' -> l
-                'R' -> r
-    let anodes = filter ((=='A').tlast) (M.keys nodes)
-    print $ part1 next "AAA"
-    print $ part2 next anodes
+main = tgetContents <&> parse >>= \p -> do
+    print $ part1 p
+    print $ part2 p
 
-parse (inst:_:nodes) =
-    (inst, M.fromList $ mk <$> nodes)
+data Problem = Problem
+    { _inst   :: Text
+    , _nnodes :: Int
+    , _aaa    :: Int
+    , _zzz    :: Int
+    , _alphas :: [Int]
+    , _zeds   :: [Int]
+    , _paths  :: UArray (Char,Int) Int
+    } deriving Show
+
+parse t = Problem
+    { _inst   = inst
+    , _nnodes = nnodes
+    , _aaa    = label "AAA"
+    , _zzz    = label "ZZZ"
+    , _alphas = endsIn 'A'
+    , _zeds   = endsIn 'Z'
+    , _paths  = listArray (('0',0),('1',nnodes-1)) $
+        map label lefts <> map label rights
+    }
   where
-    mk = mkItem . twords . tmap tr
-    mkItem [a,b,c] = (a,(b,c))
-    tr c = if isAlphaNum c then c else ' '
+    inst':_:nodes' = tlines t
+    inst = tmap tr inst'
+      where tr 'L' = '0'; tr 'R' = '1'
+    [ nodes, lefts, rights ] = transpose $ mk <$> nodes' where
+        mk = twords . tmap \c -> if isAlphaNum c then c else ' '
+    nodeIds = M.fromList $ zip nodes [0..]
+    endsIn c = M.assocs nodeIds & filter ((==c).tlast.fst) & map snd
+    label t = nodeIds M.! t
+    nnodes = M.size nodeIds
 
-part1 next = go 0 where
-    go i "ZZZ" = Just i
-    go i x = go (i+1) (next i x)
+next Problem{..} i x = _paths ! (c,x)
+  where c = tindex _inst (i `mod` tlength _inst)
 
--- TODO naive and slow "solution"
-part2 next = go 0 where
-    go i zz | all ((=='Z').tlast) zz = Just i
-    go i zz = go (i+1) $ map (next i) zz
+part1 prob@Problem{..} = go 0 _aaa where
+    go i x = if x == _zzz then i else go (i+1) (next prob i x)
+
+part2 prob@Problem{..} = runST do
+    let nalphas = length _alphas
+    zarray <- newArray (0,_nnodes-1) False :: ST s (STUArray s Int Bool)
+    for_ _zeds do \z -> writeArray zarray z True
+    ghosts <- newListArray (1, nalphas) _alphas :: ST s (STUArray s Int Int)
+    let ztest i = readArray ghosts i >>= readArray zarray
+    let loop i = ifM (allM ztest [1..nalphas]) (pure i) do
+            for_ [1..nalphas] $ \g ->
+                modifyArray ghosts g (next prob i)
+            loop (i+1)
+    loop 0
+
+-- -- TODO naive and slow "solution"
+-- part2 next = go 0 where
+--     go i zz | all ((=='Z').tlast) zz = Just i
+--     go i zz = go (i+1) $ map (next i) zz
