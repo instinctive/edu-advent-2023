61c519a427ff76d89852e62a6c2276425cfd4d6b
instinctive <177694+instinctive@users.noreply.github.com>
2023-12-12 06:45:02 -0800

    Day 11 solution

---
+++ day11.hs
@@ -0,0 +1,30 @@
+-- https://adventofcode.com/2023/day/11
+
+module Main where
+
+import qualified Data.Set as S
+
+main = tgetContents <&> parse >>=
+    each print . solve [2,1000000]
+
+parse raw =
+    [ V2 r c
+    | (r,t) <- zip [0..] $ tlines raw
+    , (c,x) <- zip [0..] $ tunpack t
+    , x == '#' ]
+
+solve mm gg =
+    ans <$> mm
+  where
+    ans m = mhd + exp * (m-1)
+    [ mhd, exp ] = map sum $ transpose
+        [ dist a b | a:bb <- tails gg, b <- bb ]
+    dist ab@(V2 a b) uv@(V2 u v) = [mhd,exp]
+      where
+        mhd = sum . abs $ ab - uv
+        exp = gaps rows a u + gaps cols b v
+        gaps xx a b | a > b = gaps xx b a
+        gaps xx a b = b - a - used xx where
+            used = length . takeWhile (<b) . dropWhile (<a)
+    rows = view _x <$> gg & S.toList . S.fromList
+    cols = view _y <$> gg & S.toList . S.fromList
