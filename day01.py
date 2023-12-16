871e81accc3859f419c471f2db44f7ebdea39257
instinctive <177694+instinctive@users.noreply.github.com>
2023-12-16 09:54:21 -0800

    Variable renaming

--- day01.py
+++ day01.py
@@ -6,21 +6,21 @@ sum1 = 0
 sum2 = 0
 
 for line in sys.stdin:
-    part1 = []
-    part2 = []
+    nums1 = []
+    nums2 = []
 
     for i in range(0,len(line)-1):
         if line[i] >= '1' and line[i] <= '9':
-            part1.append(int(line[i]))
-            part2.append(int(line[i]))
+            nums1.append(int(line[i]))
+            nums2.append(int(line[i]))
         else:
             for n,w in enumerate(words,1):
                 if line.startswith(w,i):
-                    part2.append(n)
+                    nums2.append(n)
                     break
 
-    sum1 += 10 * part1[0] + part1[-1]
-    sum2 += 10 * part2[0] + part2[-1]
+    sum1 += 10 * nums1[0] + nums1[-1]
+    sum2 += 10 * nums2[0] + nums2[-1]
 
 print(sum1)
 print(sum2)
